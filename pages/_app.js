f4e136a5e82fe0274feb8634c5b66faaf7bd8e38
gasp-admin <javiergp@live.com.ar>
2026-05-07 08:11:46 -0300

    Add service worker registration in _app.js
    Register service worker for the application.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,15 @@
+import { useEffect } from 'react'
 import '../styles/globals.css'
+
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/sw.js', { scope: '/' })
+        .then(reg => console.log('[GASP] SW registrado:', reg.scope))
+        .catch(err => console.error('[GASP] SW error:', err))
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
